99d151bcb0f65b7fb8efba4174e21b66b3237139
Darshan Pradhan <darshan.pradhan@anu.edu.au>
2022-10-13 18:16:10 +1100

    Menu Extension corrections

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -3,7 +3,7 @@ from ssg import hooks, parsers
 files = []
 
 @hooks.register("collect_files")
-def collect_files(source,site_parsers):
+def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
@@ -11,8 +11,8 @@ def collect_files(source,site_parsers):
                 files.append(path)
 
 @hooks.register("generate_menu")
-def generate_menu(html,ext):
+def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
     menu_item = lambda name, ext: template.format(name, ext, name.title())
-    menu = "\n".join([ menu_item(path.stem, ext) for path in files])
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
     return "<ul>\n{}</ul>\n{}".format(menu, html)
